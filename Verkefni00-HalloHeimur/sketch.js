e1853d8b58e310272a924388c2acceb93fc86988
FridrikIngi <frikki126@gmail.com>
2021-03-22 12:03:05 +0000

    alvöru skilaboð 2

--- Verkefni00-HalloHeimur/sketch.js
+++ Verkefni00-HalloHeimur/sketch.js
@@ -10,13 +10,15 @@ function setup(){
     fill(0);
     textSize(20);
     text("Halló heimur",100,40);
+    fill(0);
+    textSize(20)
+    text("Ég er flott verkefni",50,260);
     fill(255,69,0)
     rect(200,200,10,10);
     fill(139,0,139)
     ellipse(100,200,50,50);
     fill(255)
     rect(50,50,25,25);
-    textSize(20)
-    text("Ég er flott verkefni",100,-40);
+
 
 }
