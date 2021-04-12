0d7326e0924d20150f3cfc1bef4d95df7de73592
FridrikIngi <frikki126@gmail.com>
2021-04-12 15:38:11 +0000

    verkefni 3 fígura litalærdómur

--- Verkefni03-Fígúra/sketch.js
+++ Verkefni03-Fígúra/sketch.js
@@ -1,14 +1,45 @@
 // Hér kemur kóðinn þinn:
-
+var bukur = 100
+var loftnet = 100
+var hendur = 70
+var fætur = 100
+var litaveröld
+var h = 0
+var s = 360
+var b = 360
 
 
 function setup(){
     createCanvas(350,550);
     rectMode(CENTER);
-
+    colorMode(HSB);
     }
 
     function draw(){
-      backgrond(0,255,255)
+      background(180,12,100);
+      fill(0,0,75);
+      rect(mouseX,mouseY,100,bukur);
+      rect(mouseX,mouseY - bukur/1.3,80,60); //haus 1,3
 
+      fill(h,s,b);
+      rect(mouseX - 25,mouseY - bukur/1.2,20,20);
+      rect(mouseX + 25,mouseY - bukur/1.2,20,20);
+
+      fill(0);
+      line(mouseX - 40,mouseY - bukur,mouseX - 40,mouseY - loftnet - 30); //loftnet
+      fill(0,0,75);
+      rect(mouseX - 20,mouseY + bukur/2 + 30,fætur/5,60); //fætur
+      rect(mouseX + 20,mouseY + bukur/2 + 30,fætur/5,60); //fætur
+      //hendur
+      rect(mouseX - 50 - hendur/2,mouseY - bukur/4,hendur,15); //bukur
+      rect(mouseX + 50 + hendur/2,mouseY - bukur/4,hendur,15); //bukur
     }
+
+function mousePressed(){
+      bukur = random(70,110);
+      loftnet = random(70,110);
+      hendur = random(70,110);
+      fætur = random(70,120);
+      h = random (0,360);
+
+}
