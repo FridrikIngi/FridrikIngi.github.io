cadb3efdf7fbe061110740758e9d4ae01982f2f4
FridrikIngi <frikki126@gmail.com>
2021-04-15 11:01:15 +0000

    lykkjuákosrun

--- Verkefni04-Lykkjuáskorun/sketch.js
+++ Verkefni04-Lykkjuáskorun/sketch.js
@@ -5,25 +5,18 @@ function setup() {
   frameRate(2);
 }
 
+
 function draw() {
-    fill(random(0,180),100,100)
-    for(var i = 100; i < 300; i = i + 25)
-      rect(10,190,20,20);
-
+  fill(random(0,180),75,100);
+  for(var i = 100; i < 300; i = i + 25){
+    rect(i-10,190,20,20);
 }
-
-function draw(){
-  for(var i = 50; i < 351; i = i + 25) {
+  for(var i = 50; i < 351; i = i + 25){
     fill(random(180,360),100,100);
     ellipse(i,150,20,20);
   }
-
-  for(var i = 75; i < 326; i = i + 25) {
+  for(var i = 75; i < 326; i = i + 25){
     fill(random(180,360),100,100);
     ellipse(i,250,20,20);
-
   }
-
-
-
 }
