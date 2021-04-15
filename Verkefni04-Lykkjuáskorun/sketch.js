b75338d0dd84ec5ab577e84181366663bfea4b83
FridrikIngi <frikki126@gmail.com>
2021-04-15 10:46:14 +0000

    áksorun

--- Verkefni04-Lykkjuáskorun/sketch.js
+++ Verkefni04-Lykkjuáskorun/sketch.js
@@ -1,45 +1,29 @@
-// Hér kemur kóðinn þinn:
-var bukur = 100
-var loftnet = 100
-var hendur = 70
-var fætur = 100
-var litaveröld
-var h = 0
-var s = 360
-var b = 360
+function setup() {
+  createCanvas(400,400);
+  background(255, 215, 0);
+  colorMode(HSB);
+  frameRate(2);
+}
+
+function draw() {
+    fill(random(0,180),100,100)
+    for(var i = 100; i < 300; i = i + 25)
+      rect(10,190,20,20);
+
+}
+
+function draw(){
+  for(var i = 50; i < 351; i = i + 25) {
+    fill(random(180,360),100,100);
+    ellipse(i,150,20,20);
+  }
+
+  for(var i = 75; i < 326; i = i + 25) {
+    fill(random(180,360),100,100);
+    ellipse(i,250,20,20);
+
+  }
 
 
-function setup(){
-    createCanvas(350,550);
-    rectMode(CENTER);
-    colorMode(HSB);
-    }
-
-    function draw(){
-      background(180,12,100);
-      fill(0,0,75);
-      rect(mouseX,mouseY,100,bukur);
-      rect(mouseX,mouseY - bukur/1.3,80,60); //haus 1,3
-
-      fill(h,s,b);
-      rect(mouseX - 25,mouseY - bukur/1.2,20,20);
-      rect(mouseX + 25,mouseY - bukur/1.2,20,20);
-
-      fill(0);
-      line(mouseX - 40,mouseY - bukur,mouseX - 40,mouseY - loftnet - 30); //loftnet
-      fill(0,0,75);
-      rect(mouseX - 20,mouseY + bukur/2 + 30,fætur/5,60); //fætur
-      rect(mouseX + 20,mouseY + bukur/2 + 30,fætur/5,60); //fætur
-      //hendur
-      rect(mouseX - 50 - hendur/2,mouseY - bukur/4,hendur,15); //bukur
-      rect(mouseX + 50 + hendur/2,mouseY - bukur/4,hendur,15); //bukur
-    }
-
-function mousePressed(){
-      bukur = random(70,110);
-      loftnet = random(70,110);
-      hendur = random(70,110);
-      fætur = random(70,120);
-      h = random (0,360);
 
 }
